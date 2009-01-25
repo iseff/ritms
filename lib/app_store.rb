28772ea17c0b837a21bdcfc27ed1cb1b35884435
Ian Sefferman <iseff@iseff.com>
2009-01-25 11:15:25 -0800

    much more stable ITMS::AppStore.app(id) call, as well as added functionality for ITMS::AppStore.new which gets new apps. some other minor work

--- lib/app_store.rb
+++ lib/app_store.rb
@@ -24,6 +24,8 @@ class ITMS
     @@host = "ax.itunes.apple.com"
     @@search_path = "/WebObjects/MZSearch.woa/wa/advancedSearch?submit=seeAllLockups&media=software&entity=software&softwareTerm="
     @@app_path = "/WebObjects/MZStore.woa/wa/viewSoftware?mt=8&id="
+    @@new_path = "/WebObjects/MZStore.woa/wa/viewRoom?fcId=285114016&sortMode=2&id=25204"
+
 
     def self.search(q)
       results = []
@@ -31,10 +33,7 @@ class ITMS
       doc = REXML::Document.new(body)
       doc.root.each_element("//View/ScrollView/VBoxView/View/MatrixView/VBoxView/MatrixView/VBoxView/View/VBoxView/VBoxView/VBoxView/MatrixView/HBoxView/VBoxView/MatrixView") do |app|
         url = app.elements['GotoURL'].attributes["url"]
-        id = nil
-        URI.parse(url).query.split("&").each do |p|
-          id = p.split("=")[1] if p[0..2] == "id="
-        end
+        id = parse_id_from_url(url)
         name = app.elements['GotoURL'].attributes["draggingName"]
         image = app.elements['GotoURL'].elements['View'].elements['PictureView'].attributes["url"]
         price = app.elements['VBoxView'].elements['VBoxView'].elements['HBoxView'].elements['VBoxView[2]'].elements['Test'].elements['Buy'].elements['PictureButtonView'].attributes["alt"].split(": ")[1]
@@ -44,32 +43,93 @@ class ITMS
       results.empty? ? nil : results
     end
 
+
+
+    def self.new_apps
+      results = []
+      body = retrieve(@@new_path)
+      doc = REXML::Document.new(body)
+      doc.root.each_element("//View/ScrollView/VBoxView/View/MatrixView/View/VBoxView/HBoxView/VBoxView/VBoxView/View/VBoxView[2]/VBoxView/VBoxView/MatrixView/HBoxView/VBoxView/MatrixView") do |app|
+        name = app.elements['GotoURL'].attributes["draggingName"]
+        url = app.elements['GotoURL'].attributes["url"]
+        id = parse_id_from_url(url)
+        image = app.elements['GotoURL'].elements['View'].elements['PictureView'].attributes["url"]
+        price = category.elements['VBoxView'].elements['VBoxView'].elements['HBoxView'].elements['VBoxView'].elements['TextView'].elements['SetFontStyle'].elements['b']
+        category = app.elements['VBoxView'].elements['HBoxView[2]'].elements['TextView'].elements['SetFontStyle'].elements['GotoURL'].attributes['draggingName']
+        results << ITMS::App.from_search(name, url, image, id, price, category)
+      end
+
+      results.empty? ? nil : results
+    end
+
+
+
+
     def self.app(id)
+      id = id.to_i
       url = @@app_path + CGI::escape(id.to_s)
       body = retrieve(url)
       doc = REXML::Document.new(body)
       app = ITMS::App.new
-      a = doc.root
-      view = a.elements['View'].elements['ScrollView'].elements['VBoxView'].elements['View'].elements['MatrixView']
-      app.name = a.elements['iTunes'].text.strip
-      app.author_name = view.elements['MatrixView'].elements['VBoxView[1]'].elements['GotoURL'].attributes['draggingName'].strip
-      author_url = view.elements['MatrixView'].elements['VBoxView[1]'].elements['GotoURL'].attributes['url']
-      app.author_id = nil
-      URI.parse(author_url).query.split("&").each do |p|
-        app.author_id = p.split("=")[1] if p[0..2] == "id="
-      end
-      
-      # such a hack
-      description_unformatted = view.elements['MatrixView'].elements['VBoxView[2]'].elements['VBoxView[1]'].elements['TextView[2]'].elements['SetFontStyle'].to_s
-      r = Regexp.compile("<SetFontStyle.*?>(.*?)</SetFontStyle>", Regexp::MULTILINE)
-      app.description = r.match(description_unformatted)[1]
-      price_unformatted = view.elements['MatrixView'].elements['VBoxView'].elements['VBoxView'].elements['MatrixView'].elements['VBoxView'].elements['VBoxView'].elements['HBoxView'].elements['VBoxView'].elements['TextView'].elements['SetFontStyle'].elements['b'].to_s
-      r2 = Regexp.compile("<b>(.*?)</b>")
-      app.price = r2.match(price_unformatted)[1]
 
-      app.category = view.elements['MatrixView'].elements['VBoxView[1]'].elements['VBoxView[1]'].elements['MatrixView'].elements['VBoxView'].elements['HBoxView[2]'].elements['TextView'].elements['SetFontStyle'].text.split(": ")[1].strip
-      app.image = view.elements['MatrixView'].elements['VBoxView'].elements['VBoxView'].elements['MatrixView'].elements['GotoURL'].elements['PictureView'].attributes["url"]
-      app.screenshot_url =  view.elements['MatrixView'].elements['VBoxView'].elements['View[2]'].elements['View'].elements['View'].elements['VBoxView'].elements['View'].elements['VBoxView'].elements['HBoxView'].elements['LoadFrameURL'].elements['PictureView'].attributes["url"]
+      app.id = id
+      unless (id.is_a?(Integer) or id.is_a?(Bignum)) and id >= 1
+        raise Exception, "Invalid app id: #{id}, #{id.class}"
+      end
+
+
+      a = doc.root
+      begin
+        view = a.elements['View'].elements['ScrollView'].elements['VBoxView'].elements['View'].elements['MatrixView']
+      rescue
+        raise Exception, "Invalid page for app #{id}"
+      end
+
+      begin
+        app.name = a.elements['iTunes'].text.strip
+      rescue 
+      end
+
+      begin
+        app.author_name = view.elements['MatrixView'].elements['VBoxView[1]'].elements['GotoURL'].attributes['draggingName'].strip
+      rescue
+      end
+
+      begin
+        author_url = view.elements['MatrixView'].elements['VBoxView[1]'].elements['GotoURL'].attributes['url']
+        app.author_id = parse_id_from_url(author_url)
+      rescue
+      end
+
+      begin
+        # such a hack
+        description_unformatted = view.elements['MatrixView'].elements['VBoxView[2]'].elements['VBoxView[1]'].elements['TextView[2]'].elements['SetFontStyle'].to_s
+        r = Regexp.compile("<SetFontStyle.*?>(.*?)</SetFontStyle>", Regexp::MULTILINE)
+        app.description = r.match(description_unformatted)[1]
+      rescue
+      end
+
+      begin
+        price_unformatted = view.elements['MatrixView'].elements['VBoxView'].elements['VBoxView'].elements['MatrixView'].elements['VBoxView'].elements['VBoxView'].elements['HBoxView'].elements['VBoxView'].elements['TextView'].elements['SetFontStyle'].elements['b'].to_s
+        r2 = Regexp.compile("<b>(.*?)</b>")
+        app.price = r2.match(price_unformatted)[1]
+      rescue
+      end
+
+      begin
+        app.category = view.elements['MatrixView'].elements['VBoxView[1]'].elements['VBoxView[1]'].elements['MatrixView'].elements['VBoxView'].elements['HBoxView[2]'].elements['TextView'].elements['SetFontStyle'].text.split(": ")[1].strip
+      rescue
+      end
+
+      begin
+        app.image = view.elements['MatrixView'].elements['VBoxView'].elements['VBoxView'].elements['MatrixView'].elements['GotoURL'].elements['PictureView'].attributes["url"]
+      rescue
+      end
+
+      begin
+        app.screenshot_url =  view.elements['MatrixView'].elements['VBoxView'].elements['View[2]'].elements['View'].elements['View'].elements['VBoxView'].elements['View'].elements['VBoxView'].elements['HBoxView'].elements['LoadFrameURL'].elements['PictureView'].attributes["url"]
+      rescue
+      end
 
       return app
     end
@@ -83,6 +143,13 @@ class ITMS
         return response.plain_body
       end
     end
+
+    def self.parse_id_from_url(url)
+      URI.parse(url).query.split("&").each do |p|
+        return p.split("=")[1] if p[0..2] == "id="
+      end
+    end
+
     
   end
 end
